35303002b8b3586979f785a0f18467f07e58663f
Zhen Luo <461652354@qq.com>
2018-11-18 23:39:29 +0800

    Moved the specific version of libsvm for SSEQ into Utilities/

--- IQM/SSEQ/SSQA_by_f.m
+++ IQM/SSEQ/SSQA_by_f.m
@@ -1,10 +1,10 @@
 function score = SSQA_by_f( feature)
     load('model.mat', 'fmax', 'fmin', 'svmmodel', 'svrmodel');
     feature =setscale(feature,fmax,fmin);
-    [~, ~, p] = svmpredict(ones(1,1), feature, svmmodel,'-b 1 -q');
+    [~, ~, p] = svmpredict_old(ones(1,1), feature, svmmodel,'-b 1 -q'); % use the old version
     q=zeros(1,5);
     for j=1:5
-        q(:, j) = svmpredict(ones(1,1), feature, svrmodel{j}, '-q');
+        q(:, j) = svmpredict_old(ones(1,1), feature, svrmodel{j}, '-q'); % use the old version
     end
     Q=sum(p.*q , 2);
     score=Q*50+50;
